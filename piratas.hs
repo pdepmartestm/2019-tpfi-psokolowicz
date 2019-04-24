5faada69c34eb8f8dfd3c9a294ee8090266cb046
psokolowicz <sokolowiczpaula@gmail.com>
2019-04-24 09:21:25 -0300

    mismo que commit anterior pero con constructores

--- piratas.hs
+++ piratas.hs
@@ -1,17 +1,33 @@
-type Botin = ([Char], Integer)
-type Pirata = ([Char], [Botin])
-type Piratas = [Pirata]
+data Tesoro = UnTesoro{ nombreTesoro::[Char]
+                        , valor::Int
+                        } deriving Show
 
-listaPiratas = [("Jack Sparrow", [("Brujula que apunta lo que mas deseas", 10000), ("Frasco de arena", 0)]), ("David Jones", [("Cajita musical", 1)]), ("Anne Bonny", [("Doblones", 100), ("Frasco de arena", 1)])]
-pirataJack = ("Jack Sparrow", [("Brujula que apunta lo que mas deseas", 10000), ("Frasco de arena", 0)])
-pirataDavid = ("David Jones", [("Cajita musical", 1)])
-pirataAnne = ("Anne Bonny", [("Doblones", 100), ("Frasco de arena", 1)])
+data Pirata = UnPirata{ nombrePirata::[Char]
+                        , botin::[Tesoro]
+                        } deriving Show
+
+tesoroJack1 = (UnTesoro "Brujula que apunta lo que mas deseas" 10000)
+tesoroJack2 = (UnTesoro "Frasco de arena" 3)
+pirataJack = (UnPirata "Jack Sparrow" [tesoroJack1, tesoroJack2])
+
+tesoroDavid1 = (UnTesoro "Cajita musical" 1)
+pirataDavid = (UnPirata "David Jones" [tesoroDavid1])
+
+tesoroAnne1 = (UnTesoro "Doblones" 100)
+tesoroAnne2 = (UnTesoro "Frasco de arena" 1)
+pirataAnne = (UnPirata "Anne Bonny" [tesoroAnne1, tesoroAnne2])
 
 cantTesorosPorPirata :: Pirata -> Int
-cantTesorosPorPirata pirata = length (snd pirata)
+cantTesorosPorPirata pirata = length (botin pirata)
 
 pirataAfortunado :: Pirata -> Bool
-pirataAfortunado pirata = (sum (map (snd) (snd pirata))) > 10000
+pirataAfortunado pirata = (sum (map (valor) (botin pirata))) > 10000
+
+--igualTesoroDiferenteValor :: Botin -> Botin -> Bool
+--igualTesoroDiferenteValor botin1 botin2 = (fst botin1 == fst botin2) && (snd botin1 /= snd botin2)
+
+--loTiene :: [Botin] -> [Botin] -> Bool
+--loTiene botin1 botin2 = any (igualTesoroDiferenteValor botin1 botin2) botin2
 
 --tesoroRepetidoPeroDiferenteValor :: Pirata -> Pirata -> Bool
---tesoroRepetidoPeroDiferenteValor pirata1 pirata2 =  map (any (snd (fst pirata1)==) (snd (fst pirata2))) pirata1
+--tesoroRepetidoPeroDiferenteValor pirata1 pirata2 = any (loTiene (snd pirata1) (snd pirata2)) (snd pirata2)
