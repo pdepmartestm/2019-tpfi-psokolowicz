f5c78624f82fcd1f5407a4087e69e87f9fd35f83
psokolowicz <sokolowiczpaula@gmail.com>
2019-05-05 21:51:47 -0300

    primera parte (piratas)

--- piratas.hs
+++ piratas.hs
@@ -1,8 +1,10 @@
 data Tesoro = UnTesoro{ nombreTesoro::[Char]
                         , valor::Int
-                        }
+                        } deriving Eq
 instance Show Tesoro where
     show tesoro = "Tesoro: " ++ nombreTesoro tesoro ++ ". Valor: " ++ show (valor tesoro)
+instance Ord Tesoro where
+    compare t1 t2 = compare (valor t1) (valor t2)
 
 data Pirata = UnPirata{ nombrePirata::[Char]
                         , botin::[Tesoro]
@@ -17,7 +19,7 @@ pirataJack = (UnPirata "Jack Sparrow" [tesoroJack1, tesoroJack2])
 tesoroDavid1 = (UnTesoro "Cajita musical" 1)
 pirataDavid = (UnPirata "David Jones" [tesoroDavid1])
 
-tesoroAnne1 = (UnTesoro "Doblones" 100)
+tesoroAnne1 = (UnTesoro "Doblones" 101)
 tesoroAnne2 = (UnTesoro "Frasco de arena" 1)
 pirataAnne = (UnPirata "Anne Bonny" [tesoroAnne1, tesoroAnne2])
 
@@ -31,4 +33,12 @@ igualNombreDiferenteValor tesoro1 tesoro2 = ((nombreTesoro tesoro1) == (nombreTe
 loTiene botin1 tesoro2 = any (igualNombreDiferenteValor tesoro2) botin1
 tesoroRepetidoPeroDiferenteValor pirata1 pirata2 = any (loTiene (botin pirata1)) (botin pirata2)
 
-adquirirNuevoTesoroYMostrar pirata tesoroNuevo = (UnPirata(nombrePirata pirata) (tesoroNuevo : (botin pirata)))
+tesoroMasValioso pirata = maximum (botin pirata)
+
+adquirirNuevoTesoroYMostrar pirata tesoroNuevo = (UnPirata (nombrePirata pirata) (tesoroNuevo : (botin pirata)))
+
+tesoroNoValioso tesoro = valor tesoro < 100
+perderTesorosValiososYMostrar pirata = (UnPirata (nombrePirata pirata) (filter (tesoroNoValioso) (botin pirata)))
+
+tesoroDeNombreEspecifico nombre tesoro = nombreTesoro tesoro /= nombre
+perderTesorosDeNombreEspecifico pirata nombre = (UnPirata (nombrePirata pirata) (filter (tesoroDeNombreEspecifico nombre) (botin pirata)))
